15c9cef2e1b87b87b69db65b754f683891ea7756
Chetan Lande <160496624+Chetan-Lande@users.noreply.github.com>
2025-02-21 15:34:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ async def process_files(speaker_audio: UploadFile = File(...), input_file: Uploa
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8081)
+    uvicorn.run(app, host="0.0.0.0", port=3306)
